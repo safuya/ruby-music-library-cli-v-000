403953dacc4a40c349e6efb6fa0727d4d3b55ad1
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-02 18:58:41 +0000

    automatically backed up by learn

--- lib/concerns/music_library_controller.rb
+++ lib/concerns/music_library_controller.rb
@@ -55,6 +55,8 @@ class MusicLibraryController
     if genre
       genre.songs.sort { |a, b| a.name <=> b.name }.each_with_index do |song, i|
         puts "#{i + 1}. #{song.artist.name} - #{song.name}"
+      end
+    end
   end
 
 end
