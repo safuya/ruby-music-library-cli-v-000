e333a8f9d859da1edb8a562ad6011b03bcd416db
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-02 20:00:07 +0000

    automatically backed up by learn

--- lib/concerns/music_library_controller.rb
+++ lib/concerns/music_library_controller.rb
@@ -66,7 +66,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     song_no = gets.to_i
-    song = sorted_songs[song_no]
+    song = sorted_songs[song_no + 1]
     puts "Playing #{song.name} by #{song.artist.name}"
   end
 
