0781f353260d2082c26e40ddade90fb9ba17bb9a
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-02 20:25:03 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,7 +15,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      choice = gets
+      choice = gets.chomp
       binding.pry
     end
   end
