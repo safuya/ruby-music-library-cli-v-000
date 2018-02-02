7686cdbde181a3fbbbd5b6e89fee64762e768919
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-02 19:55:10 +0000

    automatically backed up by learn

--- lib/concerns/music_library_controller.rb
+++ lib/concerns/music_library_controller.rb
@@ -24,7 +24,7 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.sort { |a, b| a.name <=> b.name }.each_with_index do |song, i|
+    sorted_songs.each_with_index do |song, i|
       puts "#{i + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
